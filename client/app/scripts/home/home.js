eb9d005d02c38d2e76a4a8db2d8f2a677665e4b5
John Peterson <jlpeterson829@gmail.com>
2015-03-26 01:15:42 -0400

    Add functionality for displaying users you follow on a page

--- client/app/scripts/home/home.js
+++ client/app/scripts/home/home.js
@@ -14,7 +14,9 @@
   function HomeController($scope, GitApi, Auth, Chart){
     $scope.github = {};
     $scope.currentUser = {};
-    $scope.lastUser = {};
+
+    $scope.lastUser = {};  //add last user for display purposes
+    $scope.usersFollowing = [];
     $scope.loaded = false;
     $scope.following = false;  //add condition for following
     $scope.loaded3 = true;
@@ -99,8 +101,19 @@
     };
 
     $scope.follow = function(username) {
-      console.log("bout to follow!");
-      
+      console.log("about to follow!");
+      GitApi.getUserContact(username)
+        .then(function(data){
+          console.log("contact info ", data);
+          console.log("name ", data.name);
+          console.log("email ", data.email);
+          var user = {
+            username: username,
+            name: data.name,
+            email: data.email
+          }
+          $scope.usersFollowing.push(user);
+        });
     }
   }
 })();
