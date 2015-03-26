14573f5f2d79c9b7acedf1d38f94023bc6e15e3d
Daniel Miller <danielmiller.nl@gmail.com>
2015-03-26 20:29:52 -0230

    fixed database issues

--- server/Schemas/GitUsers.js
+++ server/Schemas/GitUsers.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
 var UserProfile = {
   name:String,
@@ -18,12 +17,27 @@ var UserProfile = {
   updated_at:Date,
 };
 
-var GitUserSchema = new Schema({
+var GitUserSchema = new mongoose.Schema({
     username: String,
     oauthToken: String,
     totalLinesOfCode: Number, 
     languages: [{language:String, lines:Number, percentage:Number}],
-    profileInfo: UserProfile
+    profileInfo: {
+      name:String,
+      company:String,
+      blog:String, 
+      location:String,
+      email:String,
+      hireable:Boolean,
+      bio:String,
+      public_repos:Number,
+      private_repos:Number,
+      public_gists:Number,
+      followers:Number,
+      following:Number,
+      created_at:Date,
+      updated_at:Date,
+    }
 });
 
 var GitUser = mongoose.model('GitUser', GitUserSchema);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,32 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
-require('./middleware.js')(app);
+var GitUser = require('./Schemas/GitUsers');
+var Employer = require('./Schemas/Employers');
+
+var mongoURI = process.env.MONGO || 'mongodb://localhost/gitInsights';
+mongoose.connect(mongoURI);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open',function(callback){
+  console.log("MONGODB CONNECTION OPEN");
+  
+  Employer.create({name:'test1'},function(err,model){
+    if (err) console.log(error);
+    console.log("MODEL:",model);
+  });
+
+  GitUser.create({
+    username:'testUser',
+    oauthToken: 'secretKey',
+    totalLinesOfCode: 10000 
+  },function(err,model){
+    if (err) console.log(error);
+    console.log("MODEL:",model);
+  });
+});
+
+require('./middleware.js')(app,express);
 
 module.exports = app;
