8c8fe14eda348bcbe8fe07ce27e719aa7c8f54be
John Peterson <jlpeterson829@gmail.com>
2015-03-25 23:21:38 -0400

    Add potential files/route for following user controller/view, not yet used

--- client/app/scripts/following/usersFollowing.js
+++ client/app/scripts/following/usersFollowing.js
@@ -1,1 +1,13 @@
 // controller for page of all git hub users a gitinsight user is following
+
+(function(){
+  'use strict';
+
+  angular.module('gitInsight.following', [])
+    .controller('FollowingController', FollowingController);
+
+  FollowingController.$inject = ['$scope', 'GitApi', 'Auth'];
+  function FollowingController ($scope, GitApi, Auth) {
+    console.log("in following ctrl");
+  }
+})
